e36081a9dfc47a5573cebeec5af3f235caf79caa
pardeep-paxcel <pardeep.singh@paxcel.net>
2015-05-13 13:42:47 +0530

    Added dockerfile and modified app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.get('/',function(request,response){
 	response.end("hey");
 })
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var port = 9002
+var ip_address = '127.0.0.1'
 app.listen(port, ip_address, function () {
   console.log( "Listening on " + ip_address + ", port " + port )
 });
